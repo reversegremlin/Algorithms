877314b1a06b2dd98bf6d2663ef769e4b7cd3c66
Nadia Morris <reversegremlin@gmail.com>
2020-05-13 21:11:13 -0400

    moved Node to separate file

---
+++ Algorithms/Node.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Node
+    {
+        public Node next = null;
+        public int data;
+
+        public Node(int d)
+        {
+            data = d;
+        }
+
+        public void appendToTail(int d)
+        {
+            Node end = new Node(d);
+            Node n = this;
+            while (n.next != null)
+            {
+                n = n.next;
+            }
+            n.next = end;
+        }
+
+        public Node deleteNode(Node head, int d)
+        {
+            Node n = head;
+
+            if (n.data == d)
+            {
+                return head.next;
+            }
+
+            while (n.next != null)
+            {
+                if (n.next.data == d)
+                {
+                    n.next = n.next.next;
+                    return head;
+                }
+                n = n.next;
+            }
+            return head;
+        }
+    }
+}
